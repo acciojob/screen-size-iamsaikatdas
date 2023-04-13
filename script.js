d9888311022244460fcf40a9d18af5960a579676
Saikat Das <102199984+iamsaikatdas@users.noreply.github.com>
2023-04-13 21:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let width = window.innerWidth;
 let height = window.innerHeight;
 
 wid.innerText += width;
-console.log("Initial window height:", height
+heig.innerText += height
 
 window.addEventListener("resize", () => {
   width = window.innerWidth;
   height = window.innerHeight;
-  
-  console.log("New window width:", width);
-  console.log("New window height:", height);
+
+wid.innerText = width;
+heig.innerText = height
 });
