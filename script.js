b0ba572a696bec18d7752b01d5acbc553b5d83fc
Saikat Das <102199984+iamsaikatdas@users.noreply.github.com>
2023-04-14 21:06:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ let heig = document.getElementById('heights')
 let width = window.innerWidth;
 let height = window.innerHeight;
 
-console.log("Initial window width:", width);
-console.log("Initial window height:", height);
 
 wid.innerText = width;
 heig.innerText = height
